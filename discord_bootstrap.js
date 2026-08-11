58a64fdf7f5eddb1e2de6d8c1829afb15aeb1752
Rykeen-sargeras <dstayt@gmail.com>
2026-08-11 08:48:58 -0400

    Load goinglive command guard

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -1,5 +1,8 @@
 'use strict';
 
 const { patchDiscordClient } = require('./going_live');
+const { installGuard } = require('./going_live_command_guard');
+
 patchDiscordClient();
+installGuard();
 require('./discord_bot.js');
