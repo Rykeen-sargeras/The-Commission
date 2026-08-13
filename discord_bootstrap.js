43170cb4bbd26ef4c30a625669b35d93d9d7c15e
Rykeen-sargeras <dstayt@gmail.com>
2026-08-13 04:31:24 -0400

    Load clean economy UI patch

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -6,6 +6,7 @@ const { installGuard } = require('./going_live_command_guard');
 patchDiscordClient();
 installGuard();
 require('./economy_balance_patch');
+require('./economy_ui_clean_patch');
 require('./economy_luck_store_patch');
 require('./economy_luck_rng_fix');
 require('./economy_luck_panel_patch');
