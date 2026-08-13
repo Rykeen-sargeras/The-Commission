3c7d6bf12e9853938cc7a1beee93bc6e1f1fe7a3
Rykeen-sargeras <dstayt@gmail.com>
2026-08-13 04:15:43 -0400

    Load luck shop and RNG daily patch

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -6,4 +6,5 @@ const { installGuard } = require('./going_live_command_guard');
 patchDiscordClient();
 installGuard();
 require('./economy_balance_patch');
+require('./economy_luck_store_patch');
 require('./discord_bot.js');
