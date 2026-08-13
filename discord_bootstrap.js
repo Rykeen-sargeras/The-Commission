3f65158aa00f52ac11ddee51f7b1463e95cb2d41
Rykeen-sargeras <dstayt@gmail.com>
2026-08-13 04:23:15 -0400

    Load persistent Luck Shop panel

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -8,4 +8,5 @@ installGuard();
 require('./economy_balance_patch');
 require('./economy_luck_store_patch');
 require('./economy_luck_rng_fix');
+require('./economy_luck_panel_patch');
 require('./discord_bot.js');
