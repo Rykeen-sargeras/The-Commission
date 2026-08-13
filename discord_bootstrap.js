5e66a30e1ef745ad30fda0a9a0c6e2329c108abc
Rykeen-sargeras <dstayt@gmail.com>
2026-08-13 04:34:57 -0400

    Load 3x3 slots patch

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -10,4 +10,5 @@ require('./economy_ui_clean_patch');
 require('./economy_luck_store_patch');
 require('./economy_luck_rng_fix');
 require('./economy_luck_panel_patch');
+require('./economy_slots_patch');
 require('./discord_bot.js');
