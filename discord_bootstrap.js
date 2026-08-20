07576dc8f74bb174f535355b75246a5a18547618
Rykeen-sargeras <dstayt@gmail.com>
2026-08-20 15:33:09 -0400

    Load channel permission editor bridge

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -10,4 +10,5 @@ require('./economy_ui_clean_patch');
 require('./economy_luck_store_patch');
 require('./economy_luck_rng_fix');
 require('./economy_luck_panel_patch');
+require('./permissions_bridge');
 require('./discord_bot.js');
