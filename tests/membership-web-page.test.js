6e61db215860b31da860a7c339c21835e8ac2fb3
Devon <dstayt@gmail.com>
2026-09-04 16:58:00 -0400

    Show creator links inline

--- tests/membership-web-page.test.js
+++ tests/membership-web-page.test.js
@@ -13,5 +13,7 @@ assert.doesNotThrow(
 );
 assert.match(match[1], /document\.getElementById\('add'\)/, 'add-streamer handler should use an explicit DOM reference');
 assert.match(match[1], /addButton\.onclick=/, 'add-streamer button should receive its click handler');
+assert.match(html, /Private creator setup link/, 'creator links should be rendered in the page');
+assert.doesNotMatch(match[1], /prompt\('Send this private one-time setup link/, 'creator links should not rely on a browser prompt');
 
 console.log('membership web page script test passed');
