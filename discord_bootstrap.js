b859d684281e98020dad87321a046676a84b36e0
Rykeen-sargeras <dstayt@gmail.com>
2026-09-07 02:37:10 -0400

    Enable protected-ban guard at bot startup

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -1,5 +1,8 @@
 'use strict';
 
-require('./discord_bot.js');
+const Discord = require('discord.js');
+const { installProtectedBanBootstrap } = require('./protected_bans');
 
+installProtectedBanBootstrap(Discord);
+require('./discord_bot.js');
 
