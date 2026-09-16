e2ea0bd10fd34b672c05d022370a6427b3c0bcd5
Rykeen-sargeras <dstayt@gmail.com>
2026-09-15 22:32:25 -0400

    Install heist enhancements

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -3,7 +3,9 @@
 const Discord = require('discord.js');
 const { installProtectedBanBootstrap } = require('./protected_bans');
 const { installSpecialEconomyEvents } = require('./economy_special_events');
+const { installHeistEnhancements } = require('./economy_heist_enhancements');
 
 installProtectedBanBootstrap(Discord);
 installSpecialEconomyEvents();
+installHeistEnhancements();
 require('./discord_bot.js');
