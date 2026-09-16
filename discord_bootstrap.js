dd039c65cdb4405d4be79839bc503c3be9450471
Rykeen-sargeras <dstayt@gmail.com>
2026-09-15 22:45:34 -0400

    Install Higher Lower tie patch before Discord economy loads

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -2,10 +2,14 @@
 
 const Discord = require('discord.js');
 const { installProtectedBanBootstrap } = require('./protected_bans');
+const { installHigherLowerTiePatch } = require('./economy_hilo_tie_patch');
+
+installProtectedBanBootstrap(Discord);
+installHigherLowerTiePatch();
+
 const { installSpecialEconomyEvents } = require('./economy_special_events');
 const { installHeistEnhancements } = require('./economy_heist_enhancements');
 
-installProtectedBanBootstrap(Discord);
 installSpecialEconomyEvents();
 installHeistEnhancements();
 require('./discord_bot.js');
