2fac6d9caa132cda4fadfdcfae6ebecdea7c8b90
Rykeen-sargeras <dstayt@gmail.com>
2026-09-16 08:43:16 -0400

    Install leaderboard persistence and balance channel guard

--- discord_bootstrap.js
+++ discord_bootstrap.js
@@ -11,9 +11,11 @@ const { installSpecialEconomyEvents } = require('./economy_special_events');
 const { installHeistEnhancements } = require('./economy_heist_enhancements');
 const { installHeistPersistencePatch } = require('./economy_heist_persistence_patch');
 const { installHeistPingIntervalPatch } = require('./economy_heist_ping_interval_patch');
+const { installLeaderboardGuard } = require('./economy_leaderboard_guard');
 
 installSpecialEconomyEvents();
 installHeistEnhancements();
 installHeistPersistencePatch();
 installHeistPingIntervalPatch();
+installLeaderboardGuard();
 require('./discord_bot.js');
